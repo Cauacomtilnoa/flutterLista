6340f2aebe82746d0e7bdff5fa390e439a30c72d
Cauacomtilnoa <126720628+Cauacomtilnoa@users.noreply.github.com>
2024-09-02 10:47:18 -0300

    inicio att3

--- listaatividade3/lib/components/barraNavegacao.dart
+++ listaatividade3/lib/components/barraNavegacao.dart
@@ -11,9 +11,33 @@ class _BarraNavegacaoState extends State<BarraNavegacao> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.black,
       items: [
-        
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: "Início",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.videogame_asset_rounded),
+          label: "Games",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.ondemand_video_outlined),
+          label: "Coming Soon",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.file_download_outlined),
+          label: "Downloads",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.dehaze_rounded),
+          label: "More",
+        ),
       ],
+        selectedItemColor: Colors.white,  
+        unselectedItemColor: const Color.fromARGB(255, 182, 182, 182), 
+        showUnselectedLabels: true, 
     );
   }
 }
--- listaatividade3/lib/telaInicial.dart
+++ listaatividade3/lib/telaInicial.dart
@@ -13,11 +13,42 @@ class _TelainicialState extends State<Telainicial> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Netflix", style: TextStyle(color: Color.fromARGB(255, 197, 35, 23)),),
+        title: Text(
+          "Netflix",
+          style: TextStyle(color: Color.fromARGB(255, 197, 35, 23)),
+        ),
         backgroundColor: Colors.black,
       ),
-      bottomNavigationBar:BarraNavegacao() ,
-
+      body: Container(
+        color: Colors.black,
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      "Top Trendings",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 200,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BarraNavegacao(),
     );
   }
 }
